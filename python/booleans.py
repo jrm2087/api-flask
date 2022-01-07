4b1c85b2ab296e6005c4c14ccd06fc5f1e6fed7a
José <63377749+jrm2087@users.noreply.github.com>
2022-01-06 19:25:20 -0500

    boolean -if

---
+++ python/booleans.py
@@ -0,0 +1,12 @@
+print(5 == 5)
+print(5 > 5)
+print(87 != 87)
+
+# comparisons: == , != , > ,< , >=, <=
+
+friends = ['jose', 'dario']
+abroad = ['jose', 'dario']
+
+print(friends == abroad)
+print(friends is abroad)
+print(friends is friends)
---
+++ python/if_statements.py
@@ -0,0 +1,10 @@
+day_of_week = input('Cúal día de la semana es hoy ?  ').lower()
+
+if day_of_week == 'lunes':
+    print('Feliz inicio de semana')
+elif day_of_week == 'martes':
+    print('Es Martes')
+else:
+    print('Se paso la semana !!')
+
+print("--")
