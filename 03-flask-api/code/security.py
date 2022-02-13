b11427f8eb25cca29eaaf6cc3ad3708d27155fcd
José <63377749+jrm2087@users.noreply.github.com>
2022-02-12 21:39:08 -0500

    user - userRegister

--- 03-flask-api/code/security.py
+++ 03-flask-api/code/security.py
@@ -1,20 +1,13 @@
-from werkzeug.security import safe_str_cmp
+from hmac import compare_digest
 from user import User
-
-users = [
-    User(1, 'joser', '1234')
-]
-
-username_mapping = {u.username: u for u in users}
-userid_mapping = {u.id: u for u in users}
 
 
 def authenticate(username, password):
-    user = username_mapping.get(username, None)
-    if user and safe_str_cmp(user.password, password):
+    user = User.find_by_username(username)
+    if user and compare_digest(user.password, password):
         return user
 
 
 def identity(payload):
     user_id = payload['identity']
-    return userid_mapping.get(user_id, None)
+    return User.find_by_id(user_id)
