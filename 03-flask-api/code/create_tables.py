d8ecbb1569f08940e0636f998c52fc1cbc59bd7d
José <63377749+jrm2087@users.noreply.github.com>
2022-02-13 20:56:23 -0500

    item

--- 03-flask-api/code/create_tables.py
+++ 03-flask-api/code/create_tables.py
@@ -6,5 +6,9 @@ cursor = connection.cursor()
 create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
 cursor.execute(create_table)
 
+create_table = "CREATE TABLE IF NOT EXISTS items (name text, price real)"
+cursor.execute(create_table)
+cursor.execute("INSERT INTO items VALUES('Pc Lenovo',987.55)")
+
 connection.commit()
 connection.close()
