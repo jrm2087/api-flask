01b6209def2a4d0510713b9b131afdaeb62adf4a
José <63377749+jrm2087@users.noreply.github.com>
2022-02-13 22:38:26 -0500

    items - delete

--- 03-flask-api/code/item.py
+++ 03-flask-api/code/item.py
@@ -98,5 +98,18 @@ class Item(Resource):
 
 
 class ItemList(Resource):
+    @jwt_required()
     def get(self):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM items"
+        result = cursor.execute(query)
+
+        items = []
+        for row in result:
+            items.append({'name': row[0], 'price': row[1]})
+
+        connection.close()
+
         return {'items': items}
