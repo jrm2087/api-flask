b4b2faa2e557874642d20ac53406e4b5ee901df9
José <63377749+jrm2087@users.noreply.github.com>
2022-02-16 14:36:16 -0500

    storeModel

--- 04-flask-api-SQLAlchemy/code/app.py
+++ 04-flask-api-SQLAlchemy/code/app.py
@@ -5,6 +5,7 @@ from flask_jwt import JWT
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
+from resources.store import Store, StoreList
 
 from db import db
 
@@ -22,7 +23,9 @@ def create_table():
 
 jwt = JWT(app, authenticate, identity)  # /auth
 
+api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
+api.add_resource(StoreList, '/stores')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
--- 04-flask-api-SQLAlchemy/code/resources/item.py
+++ 04-flask-api-SQLAlchemy/code/resources/item.py
@@ -6,6 +6,7 @@ from models.item import ItemModel
 class Item(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('price', type=float, required=True, help='This field cannot be left blank.')
+    parser.add_argument('store_id', type=int, required=True, help='Every item needs a store id.')
 
     @jwt_required()
     def get(self, name):
@@ -20,7 +21,7 @@ class Item(Resource):
             return {'message': f'An item with name {name} already exists.'}, 400
 
         data = Item.parser.parse_args()
-        item = ItemModel(name, data['price'])
+        item = ItemModel(name, **data)
 
         try:
             item.save_to_db()
@@ -43,7 +44,7 @@ class Item(Resource):
 
         item = ItemModel.find_by_name(name)
         if item is None:
-            item = ItemModel(name, data['price'])
+            item = ItemModel(name, **data)
         else:
             item.price = data['price']
 
