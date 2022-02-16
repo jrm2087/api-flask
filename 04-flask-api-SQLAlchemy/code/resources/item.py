dd103a080e18ac66fd68e2fc8cddcb6a7a2985fd
José <63377749+jrm2087@users.noreply.github.com>
2022-02-16 13:40:40 -0500

    SQLAlchemy usersModel

--- 04-flask-api-SQLAlchemy/code/resources/item.py
+++ 04-flask-api-SQLAlchemy/code/resources/item.py
@@ -1,4 +1,3 @@
-import sqlite3
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 from models.item import ItemModel
@@ -55,16 +54,4 @@ class Item(Resource):
 class ItemList(Resource):
     @jwt_required()
     def get(self):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM items"
-        result = cursor.execute(query)
-
-        items = []
-        for row in result:
-            items.append({'name': row[1], 'price': row[2]})
-
-        connection.close()
-
-        return {'items': items}
+        return {'items': [x.json() for x in ItemModel.query.all()]}
