21018557c6fb42777ebb4fb17381ac85151b5de3
José <63377749+jrm2087@users.noreply.github.com>
2022-03-02 11:08:04 -0500

    jwt extended logging

--- 05-flask-api-JWT-Extended/code/resources/user.py
+++ 05-flask-api-JWT-Extended/code/resources/user.py
@@ -1,14 +1,17 @@
+from flask_jwt_extended import create_access_token, create_refresh_token
 from flask_restful import Resource, reqparse
+from hmac import compare_digest
+
 from models.user import UserModel
+
+_user_parser = reqparse.RequestParser()
+_user_parser.add_argument('username', type=str, required=True, help='This field cannot be blank.')
+_user_parser.add_argument('password', type=str, required=True, help='This field cannot be blank.')
 
 
 class UserRegister(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('username', type=str, required=True, help='This field cannot be blank.')
-    parser.add_argument('password', type=str, required=True, help='This field cannot be blank.')
-
     def post(self):
-        data = UserRegister.parser.parse_args()
+        data = _user_parser.parse_args()
 
         if UserModel.find_by_username(data['username']):
             return {"message": "A user with that username already exits"}, 400
@@ -34,3 +37,17 @@ class User(Resource):
             return {'message': 'User not found'}, 404
         user.delete_from_db()
         return {'message': 'User deleted'}, 200
+
+
+class UserLogin(Resource):
+    @classmethod
+    def post(cls):
+        data = _user_parser.parse_args()
+        user = UserModel.find_by_username(data['username'])
+
+        if user and compare_digest(user.password, data['password']):
+            access_token = create_access_token(identity=user.id, fresh=True)
+            refresh_token = create_refresh_token(user.id)
+            return {'access_token': access_token, 'refresh_token': refresh_token}, 200
+
+        return {'message': 'Indalid credentials'}, 401
