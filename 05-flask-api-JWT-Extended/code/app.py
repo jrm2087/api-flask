0b48ab6ff73995167cc1c741e474933bae5bd20e
José <63377749+jrm2087@users.noreply.github.com>
2022-03-02 16:10:32 -0500

    logout

--- 05-flask-api-JWT-Extended/code/app.py
+++ 05-flask-api-JWT-Extended/code/app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
-from resources.user import UserRegister, User, UserLogin, TokenRefresh
+from resources.user import UserRegister, User, UserLogin, UserLogout, TokenRefresh
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
@@ -40,8 +40,7 @@ def add_claims_to_jwt(identity):
 
 @jwt.token_in_blocklist_loader
 def check_if_token_in_blacklist(jwt_header, jwt_payload):
-    print('-->', jwt_payload)
-    return jwt_payload['sub'] in BLACKLIST
+    return jwt_payload['jti'] in BLACKLIST
 
 
 @jwt.expired_token_loader
@@ -91,6 +90,7 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 api.add_resource(User, '/user/<int:user_id>')
 api.add_resource(UserLogin, '/login')
+api.add_resource(UserLogout, '/logout')
 api.add_resource(TokenRefresh, '/refresh')
 
 if __name__ == '__main__':
--- 05-flask-api-JWT-Extended/code/resources/user.py
+++ 05-flask-api-JWT-Extended/code/resources/user.py
@@ -1,11 +1,12 @@
 from flask_jwt_extended import (create_access_token,
                                 create_refresh_token,
                                 jwt_required,
-                                get_jwt_identity)
+                                get_jwt_identity, get_jwt)
 from flask_restful import Resource, reqparse
 from hmac import compare_digest
 
 from models.user import UserModel
+from blacklist import BLACKLIST
 
 _user_parser = reqparse.RequestParser()
 _user_parser.add_argument('username', type=str, required=True, help='This field cannot be blank.')
@@ -54,6 +55,14 @@ class UserLogin(Resource):
             return {'access_token': access_token, 'refresh_token': refresh_token}, 200
 
         return {'message': 'Indalid credentials'}, 401
+
+
+class UserLogout(Resource):
+    @jwt_required()
+    def post(self):
+        jti = get_jwt()['jti']
+        BLACKLIST.add(jti)
+        return {'message': 'Succesfully logged out.'}, 200
 
 
 class TokenRefresh(Resource):
