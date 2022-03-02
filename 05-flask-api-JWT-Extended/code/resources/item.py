12da1b604b63147a73c9740902b95cb48894a7f2
José <63377749+jrm2087@users.noreply.github.com>
2022-03-02 11:51:48 -0500

    required optional

--- 05-flask-api-JWT-Extended/code/resources/item.py
+++ 05-flask-api-JWT-Extended/code/resources/item.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import jwt_required, get_jwt
+from flask_jwt_extended import jwt_required, get_jwt, get_jwt_identity
 from models.item import ItemModel
 
 
@@ -57,6 +57,10 @@ class Item(Resource):
 
 
 class ItemList(Resource):
-    @jwt_required()
+    @jwt_required(optional=True)
     def get(self):
-        return {'items': [x.json() for x in ItemModel.find_all()]}
+        user_id = get_jwt_identity()
+        items = [x.json() for x in ItemModel.find_all()]
+        if user_id:
+            return {'items': items}, 200
+        return {'items': [item['name'] for item in items], 'message': 'More data available if you log in.'}, 200
