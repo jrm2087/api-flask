5aab426d4885398c8449bb387b5bc997db7aa41a
jrm2087 <jrm2087@gmail.com>
2023-09-10 17:18:41 -0500

    mejoras api - validaciones

--- 2023/flask-app/app.py
+++ 2023/flask-app/app.py
@@ -1,5 +1,6 @@
 import uuid
 from flask import Flask, request
+from flask_smorest import abort
 from db import items, stores
 
 app = Flask(__name__)
@@ -13,6 +14,16 @@ def get_stores():
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
+    if "name" not in store_data:
+        abort(
+            400,
+            message="Bad request, name should be included in the json payload."
+        )
+
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message=f"Store already exists.")
+
     store_id = uuid.uuid4().hex
     store = {**store_data, "id": store_id}
     stores[store_id] = store
@@ -22,8 +33,26 @@ def create_store():
 @app.post("/item")
 def create_item():
     item_data = request.get_json()
+
+    if (
+            "price" not in item_data
+            or "store_id" not in item_data
+            or "name" not in item_data
+    ):
+        abort(
+            400,
+            message="Bad request price,store_id and name should be included in the json payload.",
+        )
+
+    for item in items.values():
+        if (
+                item_data["name"] == item["name"]
+                and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message=f"Item already exists.")
+
     if item_data["store_id"] not in stores:
-        return {"message": "store not found"}, 404
+        return abort(404, message="Store not found")
 
     item_id = uuid.uuid4().hex
     item = {**item_data, "id": item_id}
@@ -40,16 +69,16 @@ def get_all_items():
 def get_store(store_id):
     try:
         return stores[store_id]
-    except keyError:
-        return {"message": "store not found"}, 404
+    except:
+        return abort(404, message="Store not found")
 
 
 @app.get("/item/<string:item_id>")
 def get_item(item_id):
     try:
         return items[item_id]
-    except keyword:
-        return {"message": "store not found"}, 404
+    except:
+        return abort(404, message="Item not found")
 
 
 if __name__ == '__main__':
