6e483195ce99d2aae0d5af32c627408e38aaab96
jrm2087 <jrm2087@gmail.com>
2023-09-18 11:37:39 -0500

    ajustes para despliegue pdn

--- 2023/flask-app/app.py
+++ 2023/flask-app/app.py
@@ -4,6 +4,7 @@ from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
+from dotenv import load_dotenv
 
 from resources.item import blp as item_blueprint
 from resources.store import blp as store_blueprint
@@ -16,6 +17,7 @@ from blocklist import BLOCKLIST
 
 def create_app(db_url=None):
     app = Flask(__name__)
+    load_dotenv()
 
     app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "Stores REST API"
